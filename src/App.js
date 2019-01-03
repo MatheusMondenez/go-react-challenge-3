5625eadcb2dc5f2b364b46d66527fe4aabf4d404
Matheus Mondenez <matheus.mondenez@superlogica.com>
2019-01-03 09:07:12 -0200

    Semi finalizao

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import Routes from './routes';
+
+import dotenv from 'dotenv';
+
+import './config/reactotron';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import 'font-awesome/css/font-awesome.css';
+import './styles.css';
 
 const App = () => (
-  <div className="App">
-    <h1>Teste</h1>
-  </div>
+  <Provider store={store}>
+    <Fragment>
+      <Routes>
+        <ToastContainer autoClose={5000} />
+      </Routes>
+    </Fragment>
+  </Provider>
 );
 
 export default App;
