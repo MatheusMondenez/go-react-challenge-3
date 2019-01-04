53e9536e57c22a0e624a7cfc8440bc705649c56b
Matheus Mondenez <matheus.mondenez@superlogica.com>
2019-01-04 14:58:09 -0200

    Limpando Map

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react';
-
 import MapGL, { Marker } from 'react-map-gl';
-
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { Creators as ModalActions } from '../../store/ducks/modal';
-
 import './styles.css';
 
-class Map extends Component {
+export default class Map extends Component {
   state = {
     viewport: {
       width: window.innerWidth,
@@ -30,6 +24,7 @@ class Map extends Component {
 
   resize = () => {
     const { viewport } = this.state;
+
     this.setState({
       viewport: {
         ...viewport,
@@ -48,7 +43,7 @@ class Map extends Component {
 
   render() {
     const { viewport: viewportState } = this.state;
-    const { users } = this.props;
+
     return (
       <MapGL
         {...viewportState}
@@ -57,27 +52,13 @@ class Map extends Component {
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
         onViewportChange={viewport => this.setState({ viewport })}
       >
-        {users.data.map(user => (
-          <Marker
-            latitude={user.cordinates.latitude}
-            longitude={user.cordinates.longitude}
-            key={user.id}
-          >
-            <img className="avatar" alt={`${user.name} Avatar`} src={user.avatar} />
-          </Marker>
-        ))}
+        <Marker
+          latitude={1}
+          longitude={1}
+        >
+          <img className="avatar" alt={`${user.name} Avatar`} src={user.avatar} />
+        </Marker>
       </MapGL>
     );
   }
 }
-
-const mapStateToProps = state => ({
-  users: state.users,
-});
-
-const mapDispatchToProps = dispatch => bindActionCreators(ModalActions, dispatch);
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Map);
